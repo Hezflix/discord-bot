cc62d514cf5f55467a755476e15c8aafacbdc38d
Kevin Harismendy <kharismendy@veepee.com>
2022-08-01 16:59:30 +0200

    feat: Beautify notification message

--- PlexNotifierrDiscord/Models/PlexNotifierrApiConfig.cs
+++ PlexNotifierrDiscord/Models/PlexNotifierrApiConfig.cs
@@ -3,5 +3,9 @@ namespace PlexNotifierrDiscord.Models
     public class PlexNotifierrApiConfig
     {
         public string Hostname { get; set; } = "";
+
+        public string PlexServerIdentifier { get; set; } = "";
+        
+        public string PlexServerHostName { get; set; } = "";
     }
 }
