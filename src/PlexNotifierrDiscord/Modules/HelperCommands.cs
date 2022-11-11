55616e95923c12d0461a610888283935bfb99e38
Kevin Harismendy <kharismendy@veepee.com>
2022-11-11 18:58:12 +0100

    feat: Add logging

--- src/PlexNotifierrDiscord/Modules/HelperCommands.cs
+++ src/PlexNotifierrDiscord/Modules/HelperCommands.cs
@@ -1,14 +1,22 @@
 using Discord;
 using Discord.Commands;
+using Microsoft.Extensions.Logging;
 
 namespace PlexNotifierrDiscord.Modules
 {
     public class HelperCommands : ModuleBase<ShardedCommandContext>
     {
+        private readonly ILogger<HelperCommands> _logger;
+
+        public HelperCommands(ILogger<HelperCommands> logger)
+        {
+            _logger = logger;
+        }
+        
         [Command("help", RunMode = RunMode.Async)]
         public async Task? Help()
         {
-            var foo = Context.User.GetAvatarUrl();
+            _logger.LogInformation($"Help requested by {Context.User.Username}");
             var embedBuilder = new EmbedBuilder()
                               .WithAuthor(Context.Client.CurrentUser.Username, Context.Client.CurrentUser.GetAvatarUrl())
                               .WithTitle("Hezping envoie une notification quand un nouvel épisode d'une série que tu regardes sort sur Hezflix")
